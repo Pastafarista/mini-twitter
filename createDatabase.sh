054d2efd3384423213cb9b9d20d2e966cc7e5b1b
Pastafarista <antonio.cabrera.landin@gmail.com>
2024-01-04 15:56:55 +0100

    fixed: Ahora el script elimina la base de datos anterior

--- createDatabase.sh
+++ createDatabase.sh
@@ -1,11 +1,15 @@
 # Authors: Antonio Cabrera y Alejandro Gómez
 # Description: Script para crear la base de datos y las tablas necesarias para el proyecto
 
+# Creamos el usuario admin con contraseña password y le damos todos los privilegios
 sudo mysql -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'password';"
 sudo mysql -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'password';"
 sudo mysql -e "GRANT ALL ON *.* TO 'admin'@'localhost';"
 sudo mysql -e "GRANT ALL ON *.* TO 'admin'@'%';"
 sudo mysql -e "flush privileges;"
+
+# Borramos la base de datos si existe y la creamos
+DROP DATABASE Twitter;
 sudo mysql -e "CREATE DATABASE Twitter;"
 
 # Creamos las tablas
